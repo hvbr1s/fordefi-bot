155e1fd190f7014ec5a70362c1c57aeeb9bc6f90
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2024-12-04 09:44:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -156,6 +156,9 @@ async def slack_events(request: Request):
         elif event.get('subtype') == 'channel_join':
             print('Ignoring, just someone joining the channel.')
             return Response(status_code=200)
+        elif event.get('username') and 'Fordefi' in event.get('username'):
+            print('Ignoring, just someone from Fordefi replying.')
+            return Response(status_code=200)
 
         user_text = event.get('text')    
         user_id = event.get('user')
