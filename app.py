675ba43de0766bd42b8042ea77c47c034b8374c7
Dan Jaheny <danjaheny@mac.home>
2026-01-09 12:57:19 +0100

    adding user

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ async def slack_events(request: Request):
             return Response(status_code=200)
 
         user_name = event.get('username', '')
-        if re.search(r'@DeanKuchel|fordefi|@hvbris|@dimakogan1|@michaelpoluy|@Ancientfish|@joshschwartz|poluy|dean|telebot|ron|@jacobgzx|@aprilXluo|@mlfigueroa89|@BenFordefi|@fmonte2|dor', user_name, re.IGNORECASE):
+        if re.search(r'@DeanKuchel|fordefi|@hvbris|@dimakogan1|@michaelpoluy|@Ancientfish|@joshschwartz|poluy|dean|telebot|ron|@jacobgzx|@aprilXluo|@mlfigueroa89|@BenFordefi|@fmonte2|dor|@Or0104', user_name, re.IGNORECASE):
             return Response(status_code=200)
 
         if not event.get('text'):
